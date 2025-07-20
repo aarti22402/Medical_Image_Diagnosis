edfe409ac1223a91e5cbd40f89eca80c9a94b161
aarti22402 <aartij613@gmail.com>
2025-07-20 23:21:25 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -4,19 +4,16 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropou
 from tensorflow.keras.optimizers import Adam
 import os
 
-# Paths to dataset folders
 train_path = 'dataset/train'
 val_path = 'dataset/val'
 
-# Image size and batch size
 IMG_SIZE = (150, 150)
 BATCH_SIZE = 32
 
-# Data generators for rescaling
 train_gen = ImageDataGenerator(rescale=1./255)
 val_gen = ImageDataGenerator(rescale=1./255)
 
-# Flow images from directories
+
 train_data = train_gen.flow_from_directory(
     train_path,
     target_size=IMG_SIZE,
@@ -30,11 +27,10 @@ val_data = val_gen.flow_from_directory(
     batch_size=BATCH_SIZE,
     class_mode='binary'
 )
-# ✅ Print this to understand label mapping
+
 print("Class indices:", train_data.class_indices)
 
 
-# CNN model definition
 model = Sequential([
     Conv2D(32, (3,3), activation='relu', input_shape=(150,150,3)),
     MaxPooling2D(2,2),
@@ -51,13 +47,10 @@ model = Sequential([
     Dense(1, activation='sigmoid')  # Binary classification
 ])
 
-# Compile model
 model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
 
-# Train model
 model.fit(train_data, validation_data=val_data, epochs=5)
 
-# Save model
 if not os.path.exists("model"):
     os.makedirs("model")
 model.save('model/medical_image_model.h5')
